b5c027a0503424f5bd1fe390a2bfa6603be232c8
Maxim Bazarov <bazarow.max@gmail.com>
2022-05-17 21:35:48 +0800

    Обновляет ReadMe;

--- hooks/useStore.ts
+++ hooks/useStore.ts
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
-import {StoreContext} from "../src/store/StoreProvider";
+import { StoreContext } from '../src/store/StoreProvider';
+import AppStore from '../src/store/AppStore';
 
-export default function useStore() {
+export default function useStore(): AppStore {
   const context = useContext(StoreContext);
   if (context === undefined) {
     throw new Error('useStore must be used within StoreProvider');
   }
-
+  // @ts-ignore
   return context;
 }
