11ed0daee5d6418d12a1dfd11efcdb773654dabd
Maxim Bazarov <bazarow.max@gmail.com>
2022-05-18 11:43:22 +0800

    Тесты сборки

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
 ## React-typescript-mobx-starter
 
 Данная сборка предназначенна для быстрого старта новых проектов.
 
-В состав сборки входит Next.js в typescript формате, 
+Сборка построена на Next.js в typescript формате, 
 в качестве стейтменджера используется Mobx. 
+
+Сборку я делал для себя, но если вдруг она вам помогла, кликните на звезду.
+
 
 ### Установка зависимостей
 
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,16 +1,19 @@
-import {observer} from "mobx-react-lite";
+import { observer } from 'mobx-react-lite';
 
-import useStore from "../../../hooks/useStore";
+import useStore from '../../../hooks/useStore';
 
 const Counter = () => {
-  const {counterStore: {
-    count, increment, decrement
-  }} = useStore()
-  return <div>
-    <button onClick={()=> {decrement()}}>минус</button>
-    <p>{count}</p>
-    <button onClick={()=> {increment()}}>плюс</button>
-  </div>
-}
+  const {
+    counterStore: { count, increment, decrement },
+  } = useStore();
 
-export default observer(Counter)
+  return (
+    <div>
+      <button onClick={decrement}>минус</button>
+      <p>{count}</p>
+      <button onClick={increment}>плюс</button>
+    </div>
+  );
+};
+
+export default observer(Counter);
